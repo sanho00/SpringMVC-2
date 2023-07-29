d4b988b1fe494aae693ed8a1a57eaf9786da25d5
김산호 <undersea9120@gmail.com>
2023-07-29 23:03:06 +0900

    Spring MVC 2편 - 16

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,18 +1,25 @@
 package hello.login;
 
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
+import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean logFilter() {
         // 필터 등록, WAS를 띄울 때 필터 같이 넣어줌
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
@@ -23,7 +30,7 @@ public class WebConfig {
         return filterRegistrationBean;
     }
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean loginCheckFilter() {
         // 필터 등록, WAS를 띄울 때 필터 같이 넣어줌
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
@@ -33,4 +40,24 @@ public class WebConfig {
 
         return filterRegistrationBean;
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())   // 인터셉터 등록
+                .order(1)                               // 인터셉터 호출 순서 지정
+                .addPathPatterns("/**")                 // 인터셉터 적용할 URL 패턴 지정
+                .excludePathPatterns("/css/**", "/*.ico", "/error"); // 인터셉터에서 지외하 패턴 지정
+
+            registry.addInterceptor(new LoginCheckInterceptor())
+                    .order(2)
+                    .addPathPatterns("/**")
+                    .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                            "/css/**", "*.ico", "/error");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
 }
