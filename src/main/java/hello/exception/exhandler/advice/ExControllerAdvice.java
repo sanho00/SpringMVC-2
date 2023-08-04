3ae09ba79f448a7111cf3bd9b36351b24edc1530
김산호 <undersea9120@gmail.com>
2023-08-04 20:22:15 +0900

    Spring MVC 2편 - 20

---
+++ src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,43 @@
+package hello.exception.exhandler.advice;
+
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+    // 이 컨트롤러 안에서 IllegalArgumentException 발생 하면 이 ExceptionHandler 가 잡고
+    // 그 다음 로직 실행 되고 정상 흐름으로 처리됨
+    // 상태 코드 바꾸고 싶으면 ResponseStatus 사용
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 요류");
+    }
+    // 정의 되어 있지 않은 다른 예외들을 모두 공통으로 처리해줌
+    // Exception 이 최상위이기 때문
+
+
+}
